1d9f17bfb812368fd19ce58e768f9da614e0b243
Samandar <samandarnayimjonov@gmail.com>
2025-04-03 14:56:43 +0500

    views update

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,10 @@
+from django.utils.text import slugify
 from rest_framework import viewsets
 from .models import Category, Tag
 from .serializers import CategorySerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
