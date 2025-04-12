29612e3b741f976ca992006971fd5bb0aea3eaab
Samandar <samandarnayimjonov@gmail.com>
2025-04-12 07:38:47 +0500

    pagination added

--- categories/admin.py
+++ categories/admin.py
@@ -6,8 +6,10 @@ from .models import Tag, Category
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,15 @@
+from django.contrib.admin.templatetags.admin_list import pagination
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from .models import Category, Tag
 from .serializers import CategorySerializer
+from .paginations import CategoryPagination
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = CategoryPagination
     permission_classes = [IsAdminUser, IsAuthenticatedOrReadOnly]
     lookup_field = 'slug'
 
