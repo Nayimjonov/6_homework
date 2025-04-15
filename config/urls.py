c95b65126e1966d8c426b484c9e8705128034351
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 15:38:41 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
-    path('api/users/', include('users.urls_profile')),
+    path('api/users/', include('users.urls')),
     path('api/categories/', include('categories.urls')),
     path('api/tags/', include('categories.urls_tags')),
     path('api/posts/', include('posts.urls')),
