1c2b88271163275672d69ddf1fe792b503079fd7
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 15:36:32 +0500

    update

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from . import views
+from .views import RegisterView, LogoutView
 
 urlpatterns = [
-    path('auth/register/', views.RegisterView.as_view(), name='register'),
-    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
-    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/me/', views.MeView.as_view(), name='me'),
-    path('users/<str:username>/', views.PublicUserProfileView.as_view(), name='user_profile'),
-    path('users/<str:username>/comments/', views.UserCommentsView.as_view(), name='user_comments'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
