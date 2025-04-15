9c8cd2db92a8c2218f9e7d5810c1dceac643961c
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 14:55:03 +0500

    serialziers full done users

--- users/serializers.py
+++ users/serializers.py
@@ -31,3 +31,23 @@ class UserSerializer(serializers.ModelSerializer):
         profile.save()
 
         return instance
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError("Parollar mos emas")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
