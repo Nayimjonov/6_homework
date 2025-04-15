43d69a4b9eb990498bd2771b1d2e2a50a2b8f67a
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 15:41:41 +0500

    update

--- categories/models.py
+++ categories/models.py
@@ -22,4 +22,11 @@ class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
 
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
 
