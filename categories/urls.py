f357deb185f6f906d93177f2477bfd7c972f53dc
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 17:24:28 +0500

    done

--- categories/urls.py
+++ categories/urls.py
@@ -1,14 +1,14 @@
-# from django.urls import path
-# from .views import (
-#     CategoryList, CategoryDetail, CategoryPosts,
-#     TagList, TagDetail, TagPosts
-# )
-#
-# urlpatterns = [
-#     path('', CategoryList.as_view(), name='category-list'),
-#     path('<str:slug>/', CategoryDetail.as_view(), name='category-detail'),
-#     path('<str:slug>/posts/', CategoryPosts.as_view(), name='category-posts'),
-#     path('tags/', TagList.as_view(), name='tag-list'),
-#     path('tags/<str:slug>/', TagDetail.as_view(), name='tag-detail'),
-#     path('tags/<str:slug>/posts/', TagPosts.as_view(), name='tag-posts'),
-# ]
+from django.urls import path
+from .views import (
+    CategoryList, CategoryDetail, CategoryPosts,
+    TagList, TagDetail, TagPosts
+)
+
+urlpatterns = [
+    path('', CategoryList.as_view(), name='category-list'),
+    path('<str:slug>/', CategoryDetail.as_view(), name='category-detail'),
+    path('<str:slug>/posts/', CategoryPosts.as_view(), name='category-posts'),
+    path('tags/', TagList.as_view(), name='tag-list'),
+    path('tags/<str:slug>/', TagDetail.as_view(), name='tag-detail'),
+    path('tags/<str:slug>/posts/', TagPosts.as_view(), name='tag-posts'),
+]
--- categories/views.py
+++ categories/views.py
@@ -1,69 +1,69 @@
-# from rest_framework import generics, permissions
-# from .models import Category, Tag
-# from .serializers import CategorySerializer, TagSerializer
-# from posts.serializers import PostListSerializer
-# from posts.models import Post
-#
-#
-# class CategoryList(generics.ListCreateAPIView):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
-#
-#     def get_permissions(self):
-#         if self.request.method == 'POST':
-#             return [permissions.IsAdminUser()]
-#         return [permissions.AllowAny()]
-#
-#
-# class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
-#     lookup_field = 'slug'
-#
-#     def get_permissions(self):
-#         if self.request.method in ['PUT', 'PATCH', 'DELETE']:
-#             return [permissions.IsAdminUser()]
-#         return [permissions.AllowAny()]
-#
-#
-# class CategoryPosts(generics.ListAPIView):
-#     serializer_class = PostListSerializer
-#
-#     def get_queryset(self):
-#         category_slug = self.kwargs['slug']
-#         return Post.objects.filter(
-#             category__slug=category_slug,
-#             status='published'
-#         ).order_by('-created_at')
-#
-#
-# class TagList(generics.ListCreateAPIView):
-#     queryset = Tag.objects.all()
-#     serializer_class = TagSerializer
-#
-#     def get_permissions(self):
-#         if self.request.method == 'POST':
-#             return [permissions.IsAdminUser()]
-#         return [permissions.AllowAny()]
-#
-#
-# class TagDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Tag.objects.all()
-#     serializer_class = TagSerializer
-#     lookup_field = 'slug'
-#
-#     def get_permissions(self):
-#         if self.request.method in ['PUT', 'PATCH', 'DELETE']:
-#             return [permissions.IsAdminUser()]
-#         return [permissions.AllowAny()]
-#
-#
-# class TagPosts(generics.ListAPIView):
-#     serializer_class = PostListSerializer
-#
-#     def get_queryset(self):
-#         tag_slug = self.kwargs['slug']
-#         return Post.objects.filter(
-#             tags__slug=tag_slug,
-#             status='published'
-#         ).order_by('-created_at')
+from rest_framework import generics, permissions
+from .models import Category, Tag
+from .serializers import CategorySerializer, TagSerializer
+from posts.serializers import PostSerializer
+from posts.models import Post
+
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class CategoryPosts(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        category_slug = self.kwargs['slug']
+        return Post.objects.filter(
+            category__slug=category_slug,
+            status='published'
+        ).order_by('-created_at')
+
+
+class TagList(generics.ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    lookup_field = 'slug'
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+
+class TagPosts(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['slug']
+        return Post.objects.filter(
+            tags__slug=tag_slug,
+            status='published'
+        ).order_by('-created_at')
