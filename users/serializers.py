0034aa7dbb778425fff4d77cc48b62beeb898588
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 15:32:52 +0500

    update

--- users/serializers.py
+++ users/serializers.py
@@ -1,47 +1,81 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from .models import UserProfile
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('bio', 'profile_picture', 'website')
+        fields = ['bio', 'profile_picture', 'website']
+
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'profile']
+        read_only_fields = ['id']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password2', 'email', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
     profile = UserProfileSerializer()
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'profile')
+        fields = ['email', 'first_name', 'last_name', 'profile']
 
     def update(self, instance, validated_data):
         profile_data = validated_data.pop('profile', {})
-        profile = instance.userprofile
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+
+        # Update User model fields
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.save()
-        for attr, value in profile_data.items():
-            setattr(profile, attr, value)
+
+        # Update UserProfile fields
+        profile = instance.profile
+        profile.bio = profile_data.get('bio', profile.bio)
+        profile.website = profile_data.get('website', profile.website)
+
+        if profile_data.get('profile_picture'):
+            profile.profile_picture = profile_data.get('profile_picture')
+
         profile.save()
         return instance
-
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name')
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError("Parollar mos emas")
-        return attrs
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
-        return user
