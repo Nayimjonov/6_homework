c0c0608e87fd67fff561276bdf45bae676175721
Samandar <samandarnayimjonov@gmail.com>
2025-04-15 15:32:23 +0500

    update

---
+++ users/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+class IsPostAuthorOrAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.author == request.user or request.user.is_staff
+
+class IsCommentAuthorOrPostAuthorOrAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return (obj.author == request.user or
+                obj.post.author == request.user or
+                request.user.is_staff)
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
-
 urlpatterns = [
-    path('register/', views.UserCreateView.as_view(), name='user_create'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('auth/register/', views.RegisterView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/me/', views.MeView.as_view(), name='me'),
+    path('users/<str:username>/', views.PublicUserProfileView.as_view(), name='user_profile'),
+    path('users/<str:username>/comments/', views.UserCommentsView.as_view(), name='user_comments'),
 ]
